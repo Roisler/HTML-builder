dddabcfdc83c7f5296a31736d965ab425303dd72
Roisler <roislerxs@gmail.com>
2024-01-21 10:55:59 +0700

    feat: implement solution 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const process = require('process');
+const readline = require('readline');
+
+const writeStream = fs.createWriteStream('02-write-file');
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+const endWrite = () => {
+  console.log('Спасибо за проверку задания!');
+  rl.close();
+};
+
+rl.setPrompt(
+  'Пожалуйста, введите текст, который должен быть записан в файл:\n',
+);
+rl.prompt();
+
+rl.on('line', (text) => {
+  if (text === 'exit') {
+    endWrite();
+  } else {
+    writeStream.write(`${text}\n`);
+  }
+});
+
+rl.on('SIGINT', endWrite);
