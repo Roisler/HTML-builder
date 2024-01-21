785a779aa2903ed68da448d4e7b4f03d761c4e7f
Roisler <roislerxs@gmail.com>
2024-01-21 16:56:36 +0700

    fix: add argument outputFilePath of mergeStyles func

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,25 +5,25 @@ const path = require('path');
 const directoryStylesPath = path.join(__dirname, 'styles');
 const bundleFilePath = path.join(__dirname, 'project-dist', 'bundle.css');
 
-const mergeStyles = async (dirpath) => {
+const mergeStyles = async (dirpath, outputFilePath) => {
   try {
     const stylesArray = [];
     const files = await fsp.readdir(dirpath);
     for (const file of files) {
       const fileExtension = path.extname(file);
       if (fileExtension === '.css') {
-        const filepath = path.join(directoryStylesPath, file);
+        const filepath = path.join(dirpath, file);
         const content = await fsp.readFile(filepath);
         stylesArray.push(content);
       }
     }
 
-    await fsp.writeFile(bundleFilePath, '');
+    await fsp.writeFile(outputFilePath, '');
     const stylesString = stylesArray.join('\n');
-    await fsp.appendFile(bundleFilePath, stylesString);
+    await fsp.appendFile(outputFilePath, stylesString);
   } catch (err) {
     console.log(err);
   }
 };
 
-mergeStyles(directoryStylesPath);
+mergeStyles(directoryStylesPath, bundleFilePath);
