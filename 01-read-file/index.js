afd722cbe6c0f661f0061ec62ececbed6e805bf3
Roisler <roislerxs@gmail.com>
2024-01-21 09:45:22 +0700

    fix: replacing the output method

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const path = require('path');
+const { stdout } = require('process');
 
 const filePath = path.join(__dirname, 'text.txt');
 
 const stream = fs.createReadStream(filePath, { encoding: 'UTF-8' });
 
-stream.on('data', (data) => console.log(data));
+stream.on('data', (data) => stdout.write(data));
